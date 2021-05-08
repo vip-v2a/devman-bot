430d3b07647fae98cdc1835b544948ad43eda0d7
Vv Vv <>
2021-05-08 20:49:55 +0000

    more pythonic code. Improvements

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ CHAT_ID = os.environ['CHAT_ID_TELEGRAM']
 TIMEOUT = 91
 SLEEP_PERIOD = 60 * 10
 
-counter_connection_error = 0
-last_timestamp = None
 url = 'https://dvmn.org/api/long_polling/'
 headers = {
     'Authorization': TOKEN_DEVMAN
@@ -19,15 +17,13 @@ headers = {
 
 bot = telegram.Bot(token=TOKEN_BOT)
 
-while True:
-    try:
-        if not last_timestamp:
-            response = requests.get(
-                url,
-                headers=headers,
-                timeout=TIMEOUT
-                )
-        else:
+
+def main():
+    last_timestamp = None
+    counter_connection_error = 0
+
+    while True:
+        try:
             response = requests.get(
                 url,
                 headers=headers,
@@ -35,32 +31,35 @@ while True:
                 params={'timestamp': last_timestamp}
                 )
 
-        data = response.json()
-        current_status = data['status']
+            devman_checking = response.json()
+            current_status = devman_checking['status']
 
-        if current_status == 'timeout':
-            last_timestamp = data['timestamp_to_request']
+            if current_status == 'timeout':
+                last_timestamp = devman_checking['timestamp_to_request']
 
-        if current_status == 'found':
-            title = data['new_attempts'][0]['lesson_title']
-            lesson_url = 'https://dvmn.org' + data['new_attempts'][0]['lesson_url']
-            last_timestamp = data['last_attempt_timestamp']
-            text_result = 'К сожалению, в работе нашлись ошибки'
+            if current_status == 'found':
+                title = devman_checking['new_attempts'][0]['lesson_title']
+                lesson_url = f"https://dvmn.org{devman_checking['new_attempts'][0]['lesson_url']}"
+                last_timestamp = devman_checking['last_attempt_timestamp']
+                text_result = 'К сожалению, в работе нашлись ошибки'
 
-            if not data['new_attempts'][0]['is_negative']:
-                text_result = 'Преподавателю всё понравилось, можно приступать к следующему уроку'
-            bot.send_message(
-                chat_id=CHAT_ID,
-                text=f"У Вас проверили работу '{title}'\n{lesson_url}\n\n{text_result}"
-                )
-        print(data)
-    except requests.exceptions.ReadTimeout:
-        logger.debug('нет ответа')
-    except requests.exceptions.ConnectionError:
-        # В случае многократных неудачных попыток установить соединение
-        # с сервером скрипт замедлит свою работу — возьмёт небольшую паузу.
-        counter_connection_error += 1
-        if counter_connection_error > 2:
-            logger.info('Несколько неудачных подключений подряд. Ждем 10 минут')
-            time.sleep(SLEEP_PERIOD)
-            counter_connection_error = 0
+                if not devman_checking['new_attempts'][0]['is_negative']:
+                    text_result = 'Преподавателю всё понравилось, можно приступать к следующему уроку'
+                bot.send_message(
+                    chat_id=CHAT_ID,
+                    text=f"У Вас проверили работу '{title}'\n{lesson_url}\n\n{text_result}"
+                    )
+
+        except requests.exceptions.ReadTimeout:
+            logger.info('Нет ответа за TIMEOUT')
+        except requests.exceptions.ConnectionError:
+            # В случае многократных неудачных попыток установить соединение
+            # с сервером скрипт замедлит свою работу — возьмёт небольшую паузу.
+            counter_connection_error += 1
+            if counter_connection_error > 2:
+                logger.info('Несколько неудачных подключений подряд. Ждем 10 минут')
+                time.sleep(SLEEP_PERIOD)
+                counter_connection_error = 0
+
+if __name__ == '__main__':
+    main()
